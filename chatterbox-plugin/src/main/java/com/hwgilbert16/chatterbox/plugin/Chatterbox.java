08a73407e7d1bc9887e96239131b73a513ca4b4d
zerxal <44120703+hwgilbert16@users.noreply.github.com>
2021-10-07 12:26:22 -0400

    Reimplement Socket.io Maven package

--- chatterbox-plugin/src/main/java/com/hwgilbert16/chatterbox/plugin/Chatterbox.java
+++ chatterbox-plugin/src/main/java/com/hwgilbert16/chatterbox/plugin/Chatterbox.java
@@ -1,11 +1,16 @@
 package com.hwgilbert16.chatterbox.plugin;
 
+import io.socket.client.IO;
+import io.socket.client.Socket;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import com.hwgilbert16.chatterbox.plugin.listeners.*;
 
+import java.net.URISyntaxException;
+
 public class Chatterbox extends JavaPlugin {
     public static Chatterbox plugin;
+    public Socket socket;
 
     @Override
     public void onEnable() {
@@ -14,21 +19,38 @@ public class Chatterbox extends JavaPlugin {
         // Config file setup
         FileConfiguration config = this.getConfig();
 
-        config.addDefault("webhook-url", "blank");
+        config.addDefault("webhook-url", "EMPTY");
+        config.addDefault("discord-bot-enabled", true);
+        config.addDefault("discord-bot-hostname", "EMPTY");
+        config.addDefault("discord-bot-token", "EMPTY");
         config.options().copyDefaults(true);
         saveConfig();
 
         // Check to make sure user setup the config file
-        if (config.getString("webhook-url").equals("blank")) {
+        if (config.getString("webhook-url").equals("EMPTY")) {
             getLogger().severe("You need to configure your webhook URL in config.yml for Chatterbox to work - disabling");
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
 
-        getLogger().info(getConfig().getString("webhook-url"));
+        if (config.getBoolean("discord-bot-enabled")) {
+            if (config.getString("discord-bot-hostname").equals("EMPTY") || config.getString("discord-bot-token").equals("EMPTY")) {
+                getLogger().severe("You need to configure your discord bot hostname and token in config.yml for Chatterbox two-way communication to work - disabling. If you don't want two-way communication, disable discord-bot-enabled in plugin.yml");
+                getServer().getPluginManager().disablePlugin(this);
+                return;
+            }
+        }
 
         // Event initialization
         getServer().getPluginManager().registerEvents(new AsyncPlayerChatEventListener(), this);
+
+        // Socket.IO initialization
+        IO.Options options = IO.Options.builder().build();
+        try {
+            socket = IO.socket(config.getString("discord-bot-hostname"), options);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
