13d3157350d14ec8c9338d20d51be0eb5a3bd7e1
zerxal <44120703+hwgilbert16@users.noreply.github.com>
2021-10-10 21:15:40 -0400

    Add ID to username for messages outbound from Discord and .env creation

--- chatterbox-bot/events/message.ts
+++ chatterbox-bot/events/message.ts
@@ -4,12 +4,28 @@ module.exports = {
     // @ts-ignore
     execute(message, io) {
         if (message.channelId === process.env.CHANNEL_ID && !message.author.bot) {
-            //const guild = message.client.guilds.get(message.guildId);
-            //const guildMember = message.client.guilds.cache.get(message.author.id);
-            //const guildMember = guild.member(message.author);
-            //console.log(message.client.guilds.cache.toString());
-            //io.emit("message", `<${message.author.username}> ${message.content}`);
-            io.emit("message", `<${message.member.displayName}> ${message.content}`);
+            let finalMessage: string = message.content;
+
+            // Count number of mentions in the message
+            const expression = /<@.[0-9]+>/g;
+            let match: RegExpExecArray | null;
+
+            while ((match = expression.exec(message.content)) != null) {
+                // Get user ID, including < signs
+                const userIdFull = message.content.substring(match.index, match.index + 22);
+
+                // Get standalone user ID, only the numbers
+                const userId = message.content.substring(match.index + 3, match.index + 21);
+
+                // Get username of the mentioned user
+                const username = message.client.users.cache.get(userId).username;
+
+                // Replace the full user ID with the username and an @ symbol
+                finalMessage = finalMessage.replaceAll(userIdFull, "@" + username);
+            }
+
+            // Send the complete message to the plugin to be sent in the chat
+            io.emit("message", `<${message.member.displayName}> ${finalMessage}`);
         }
     }
 }
--- chatterbox-bot/index.ts
+++ chatterbox-bot/index.ts
@@ -9,10 +9,20 @@ const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith('.js'
 
 const io: Server = new Server(3000, {});
 
+// Create .env if it doesn't exist
+if (!fs.existsSync(__dirname + "/.env")) {
+    fs.openSync(__dirname + "/.env", "w");
+
+    fs.appendFileSync(__dirname + "/.env", "DISCORD_TOKEN=\nCHANNEL_ID=\nAUTH_TOKEN=");
+}
+
+if (!process.env.DISCORD_TOKEN || !process.env.CHANNEL_ID || !process.env.AUTH_TOKEN) {
+    console.error(".env file is not configured! Fill in relevant values before starting bot.");
+    process.exit(9);
+}
+
+// Check to make sure auth is correct
 io.on("connection", (socket) => {
-    console.log(socket.handshake.auth);
-    console.log(socket.handshake.auth['auth-token']);
-    console.log(process.env.AUTH_TOKEN);
     if (socket.handshake.auth['auth-token'] !== process.env.AUTH_TOKEN) {
         console.log("Invalid auth token, disconnecting");
         socket.disconnect();
@@ -22,6 +32,7 @@ io.on("connection", (socket) => {
     console.log("Client connected");
 });
 
+// Setup events
 for (const file of eventFiles) {
     const event = require(`./events/${file}`);
     if (event.once) {
